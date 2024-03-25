43d75424b60432a6e72fbec0719e18326c1fe685
K Subiksha Shrini <139609014+subikshashrini@users.noreply.github.com>
2024-03-25 15:01:47 +0530

    Update Main.java
    i have added the question for this code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
-
+/*Create a class named "SavingsAccount" with a static data member named "annualInterestRate" to store the annual interest rate for each saver.
+    Each member of the class should have a private data member "savingsBalance" indicating the amount the saver currently has on deposit. 
+    Implement a member function named "calculateMonthlyInterest" that calculates the monthly interest by multiplying the balance by the 
+    annualInterestRate divided by 12, and adds this interest to the savingsBalance. Provide a static member function
+    named "modifyInterestRate" that sets the static annualInterestRate to a new value. 
+    Write a driver program to test the class "SavingsAccount". 
+    Instantiate two different objects of class SavingsAccount, saver1 and saver2, with balances of $2000.00 and $3000.00, respectively. 
+    Set the annualInterestRate to 3 percent. Calculate the monthly interest and print the new balances for each of the savers.
+    Then, set the annualInterestRate to 4 percent, calculate the next month's interest, and print the new balances for each of the savers.*/
 public class Main {
     public static void main(String[] args) {
 
